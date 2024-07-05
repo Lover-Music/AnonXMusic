40f6aab3a4f8562feea1b449830818bc6b4c6b13
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2024-07-05 10:43:11 +0530

    Update help.py
    Signed-off-by: Lover-Music <123320837+Lover-Music@users.noreply.github.com>

--- AnonXMusic/utils/inline/help.py
+++ AnonXMusic/utils/inline/help.py
@@ -18,64 +18,93 @@ def help_pannel(_, START: Union[bool, int] = None):
         [
             [
                 InlineKeyboardButton(
-                    text="💥ᴀᴅᴍɪɴ",
+                    text=_["H_B_1"],
                     callback_data="help_callback hb1",
                 ),
                 InlineKeyboardButton(
-                    text="💥ᴀᴜᴛʜ💥",
+                    text=_["H_B_2"],
                     callback_data="help_callback hb2",
                 ),
                 InlineKeyboardButton(
-                    text="💥ʙʟᴀᴄᴋʟɪsᴛ💥",
+                    text=_["H_B_3"],
                     callback_data="help_callback hb3",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="💥ʙʀᴏᴀᴅᴄᴀsᴛ☀️",
+                    text=_["H_B_4"],
                     callback_data="help_callback hb4",
                 ),
                 InlineKeyboardButton(
-                    text="💥ɢʙᴀɴ💥",
-                    callback_data="help_callback hb12",
-                ),
-                InlineKeyboardButton(
-                    text="💥ʟʏʀɪᴄs💥",
+                    text=_["H_B_5"],
                     callback_data="help_callback hb5",
                 ),
-            ],
-            [
                 InlineKeyboardButton(
-                    text="💥ᴩɪɴɢ💥",
-                    callback_data="help_callback hb7",
-                ),
-                InlineKeyboardButton(
-                    text="💥ᴩʟᴀʏ💥",
-                    callback_data="help_callback hb8",
-                ),
-                InlineKeyboardButton(
-                    text="💥ᴩʟᴀʏʟɪsᴛ💥",
+                    text=_["H_B_6"],
                     callback_data="help_callback hb6",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="💥ᴠɪᴅᴇᴏᴄʜᴀᴛs💥",
+                    text=_["H_B_7"],
+                    callback_data="help_callback hb7",
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_8"],
+                    callback_data="help_callback hb8",
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_9"],
+                    callback_data="help_callback hb9",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_10"],
                     callback_data="help_callback hb10",
                 ),
                 InlineKeyboardButton(
-                    text="💥sᴛᴀʀᴛ💥",
+                    text=_["H_B_11"],
                     callback_data="help_callback hb11",
                 ),
                 InlineKeyboardButton(
-                    text="💥sᴜᴅᴏ💥",
-                    callback_data="help_callback hb9",
+                    text=_["H_B_12"],
+                    callback_data="help_callback hb12",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_13"],
+                    callback_data="help_callback hb13",
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_14"],
+                    callback_data="help_callback hb14",
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_15"],
+                    callback_data="help_callback hb15",
                 ),
             ],
             mark,
         ]
     )
     return upl
+
+
+def help_back_markup(_):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["BACK_BUTTON"],
+                    callback_data=f"settings_back_helper",
+                ),
+            ]
+        ]
+    )
+    return upl
+
 
 def private_help_panel(_):
     buttons = [
